c7e9882e44022227f7aab0a88002c22c92875e7c
erikmiqueias <erikmiqueiaspereira@outlook.com>
2024-11-23 19:48:58 -0400

    feat: add header with pigz logo and button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import "./App.css";
+import pigzLogo from "./assets/Pigz Gestão - Logo.svg";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <header className="flex justify-between my-3 mx-2">
+        <img src={pigzLogo} alt="Pigz logo" />
+        <Button className="bg-orange-500 rounded-3xl h-8 w-40">
+          Já sou parceiro
+        </Button>
+      </header>
     </>
   );
 }
