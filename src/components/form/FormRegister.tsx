2201ca95a5869d32743967bdc18ea26f7781a465
erikmiqueias <erikmiqueiaspereira@outlook.com>
2024-12-07 00:41:21 -0400

    refactor: changed conditional render FormRegister

--- src/components/form/FormRegister.tsx
+++ src/components/form/FormRegister.tsx
@@ -38,125 +38,22 @@ const FormRegister = () => {
                 Dê o primeiro passo para aumentar suas vendas
               </p>
             </div>
-          </>
-        )}
 
-        {changeStep === 1 && (
-          <>
-            <h3 className="font-semibold text-[28px]">Onde fica sua loja?</h3>
-          </>
-        )}
+            <form className="flex flex-col gap-3 mt-10">
+              <label className="text-[12px] font-medium">Nome</label>
+              <input
+                className="w-[334px] h-[48px] rounded-[16px] border-solid border-[1px] border-[#999] pl-4 text-[14px]"
+                type="text"
+                placeholder="Leonercio Goesfeeld"
+              />
 
-        {changeStep === 2 && (
-          <>
-            <h3 className="font-semibold text-[28px] w-[320px]">
-              Me conte um pouco sobre sua loja
-            </h3>
-          </>
-        )}
+              <label className="text-[12px] font-medium">E-mail</label>
+              <input
+                className="w-[334px] h-[48px] rounded-[16px] border-solid border-[1px] border-[#999] pl-4 text-[14px]"
+                type="email"
+                placeholder="leonercio.goesfeeld@email.com"
+              />
 
-        <form className="flex flex-col gap-4 mt-6">
-          <div className="flex flex-col gap-1">
-            {changeStep === 0 && (
-              <>
-                <label className="text-[12px] font-medium">Nome</label>
-                <input
-                  className="w-[334px] h-[48px] rounded-[16px] border-solid border-[1px] border-[#999] pl-4 text-[14px]"
-                  type="text"
-                  placeholder="Leonercio Goesfeeld"
-                />
-              </>
-            )}
-
-            {changeStep === 1 && (
-              <>
-                <label className="text-[12px] font-medium">CEP</label>
-                <input
-                  className="w-[334px] h-[48px] rounded-[16px] border-solid border-[1px] border-[#999] pl-4 text-[14px]"
-                  type="text"
-                  placeholder="Leonercio Goesfeeld"
-                />
-              </>
-            )}
-
-            {changeStep === 2 && (
-              <>
-                <label className="text-[12px] font-medium">Nome da loja</label>
-                <input
-                  className="w-[334px] h-[48px] rounded-[16px] border-solid border-[1px] border-[#999] pl-4 text-[14px]"
-                  type="text"
-                  placeholder="Leonercio Goesfeeld"
-                />
-              </>
-            )}
-          </div>
-
-          <div className="flex flex-col gap-1">
-            {changeStep === 0 && (
-              <>
-                <label className="text-[12px] font-medium">E-mail</label>
-                <input
-                  className="w-[334px] h-[48px] rounded-[16px] border-solid border-[1px] border-[#999] pl-4 text-[14px]"
-                  type="email"
-                  placeholder="leonercio.goesfeeld@email.com"
-                />
-              </>
-            )}
-
-            {changeStep === 1 && (
-              <>
-                <div className="flex gap-4">
-                  <div>
-                    <label className="text-[12px] font-medium">Estado</label>
-                    <Select>
-                      <SelectTrigger className="w-[114px] h-[48px] rounded-[16px] border-solid border-[1px] border-[#999]" />
-                    </Select>
-                  </div>
-                  <div>
-                    <label className="text-[12px] font-medium">Cidade</label>
-                    <Select>
-                      <SelectTrigger className="w-[205px] h-[48px] rounded-[16px] border-solid border-[1px] border-[#999]" />
-                    </Select>
-                  </div>
-                </div>
-
-                <label className="text-[12px] font-medium mt-3">Endereço</label>
-                <input
-                  className="w-[334px] h-[48px] rounded-[16px] border-solid border-[1px] border-[#999] pl-4 text-[14px]"
-                  type="text"
-                  placeholder="Avenida Brasil"
-                />
-              </>
-            )}
-
-            {changeStep === 2 && (
-              <>
-                <label className="text-[12px] font-medium mt-3">
-                  CNPJ da loja
-                </label>
-                <input
-                  className="w-[334px] h-[48px] rounded-[16px] border-solid border-[1px] border-[#999] pl-4 text-[14px]"
-                  type="text"
-                  placeholder="12.345.678/0001-99"
-                />
-
-                <label className="text-[12px] font-medium mt-6">
-                  Tipo de loja
-                </label>
-                <Select>
-                  <SelectTrigger className="w-[334px] h-12 rounded-[16px] border-solid border-[1px] border-[#999]">
-                    <SelectValue
-                      className="text-[#999]"
-                      placeholder="Selecione"
-                    />
-                  </SelectTrigger>
-                </Select>
-              </>
-            )}
-          </div>
-
-          {changeStep === 0 && (
-            <>
               <div>
                 <label className="text-[12px] font-medium">Telefone</label>
                 <div className="flex">
@@ -181,10 +78,51 @@ const FormRegister = () => {
                   placeholder="(95) 99876-5432"
                 />
               </div>
-            </>
-          )}
-          {changeStep === 1 && (
-            <>
+
+              <span className="w-[334px] text-[13px] text-start">
+                Ao continuar, aceito que a Pigz entre em contato comigo por
+                telefone, e-mail ou WhatsApp.
+              </span>
+            </form>
+          </>
+        )}
+
+        {changeStep === 1 && (
+          <>
+            <h3 className="font-semibold text-[28px] self-start ml-6">
+              Onde fica sua loja?
+            </h3>
+
+            <form className="flex flex-col gap-3 mt-7">
+              <label className="text-[12px] font-medium">CEP</label>
+              <input
+                className="w-[334px] h-[48px] rounded-[16px] border-solid border-[1px] border-[#999] pl-4 text-[14px]"
+                type="text"
+                placeholder="Leonercio Goesfeeld"
+              />
+
+              <div className="flex gap-4">
+                <div>
+                  <label className="text-[12px] font-medium">Estado</label>
+                  <Select>
+                    <SelectTrigger className="w-[114px] h-[48px] rounded-[16px] border-solid border-[1px] border-[#999]" />
+                  </Select>
+                </div>
+                <div>
+                  <label className="text-[12px] font-medium">Cidade</label>
+                  <Select>
+                    <SelectTrigger className="w-[205px] h-[48px] rounded-[16px] border-solid border-[1px] border-[#999]" />
+                  </Select>
+                </div>
+              </div>
+
+              <label className="text-[12px] font-medium mt-3">Endereço</label>
+              <input
+                className="w-[334px] h-[48px] rounded-[16px] border-solid border-[1px] border-[#999] pl-4 text-[14px]"
+                type="text"
+                placeholder="Avenida Brasil"
+              />
+
               <div className="flex gap-3">
                 <div className="flex flex-col gap-1">
                   <label className="text-[12px] font-medium mt-3">Número</label>
@@ -206,34 +144,63 @@ const FormRegister = () => {
                   />
                 </div>
               </div>
-            </>
-          )}
+            </form>
+          </>
+        )}
 
-          {changeStep === 0 && (
-            <>
-              <span className="w-[334px] text-[13px] text-start">
-                Ao continuar, aceito que a Pigz entre em contato comigo por
-                telefone, e-mail ou WhatsApp.
-              </span>
-            </>
-          )}
+        {changeStep === 2 && (
+          <>
+            <h3 className="font-semibold text-[28px] w-[320px] justify-self-start">
+              Me conte um pouco sobre sua loja
+            </h3>
 
-          <Button
-            type="button"
-            onClick={() => onNext()}
-            className={`w-[334px] rounded-[16px] bg-orange-600 text-white h-12 ${
-              changeStep === 0
-                ? "mt-[67px]"
-                : changeStep === 1
-                ? "mt-12"
-                : "mt-20"
-            }`}
-          >
-            {changeStep === 2 && "Concluir"}
-            {changeStep === 1 && "Próximo"}
-            {changeStep === 0 && "Continuar"}
-          </Button>
-        </form>
+            <form className="flex flex-col gap-2 mt-[38px]">
+              <label className="text-[12px] font-medium">Nome da loja</label>
+              <input
+                className="w-[334px] h-[48px] rounded-[16px] border-solid border-[1px] border-[#999] pl-4 text-[14px]"
+                type="text"
+                placeholder="Leonercio Goesfeeld"
+              />
+
+              <label className="text-[12px] font-medium mt-3">
+                CNPJ da loja
+              </label>
+              <input
+                className="w-[334px] h-[48px] rounded-[16px] border-solid border-[1px] border-[#999] pl-4 text-[14px]"
+                type="text"
+                placeholder="12.345.678/0001-99"
+              />
+
+              <label className="text-[12px] font-medium mt-6">
+                Tipo de loja
+              </label>
+              <Select>
+                <SelectTrigger className="w-[334px] h-12 rounded-[16px] border-solid border-[1px] border-[#999]">
+                  <SelectValue
+                    className="text-[#999]"
+                    placeholder="Selecione"
+                  />
+                </SelectTrigger>
+              </Select>
+            </form>
+          </>
+        )}
+
+        <Button
+          type="button"
+          onClick={() => onNext()}
+          className={`w-[334px] rounded-[16px] bg-orange-600 text-white h-12 ${
+            changeStep === 0
+              ? "mt-[67px]"
+              : changeStep === 1
+              ? "mt-12"
+              : "mt-20"
+          }`}
+        >
+          {changeStep === 2 && "Concluir"}
+          {changeStep === 1 && "Próximo"}
+          {changeStep === 0 && "Continuar"}
+        </Button>
       </div>
     </>
   );
