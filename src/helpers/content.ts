87cedb56762601ac6f708da2f8ca4ff22a7f6cb3
erikmiqueias <erikmiqueiaspereira@outlook.com>
2024-12-07 00:21:42 -0400

    refactor: add image attribute in content.ts

--- src/helpers/content.ts
+++ src/helpers/content.ts
@@ -3,21 +3,25 @@ export const content = {
     title: "Pigz Marketplace",
     description:
       "Além de uma página exclusiva, com o Pigz Marketplace sua loja terá mais visibilidade, no app e no site. Uma vitrine com milhares de clientes todos os dias pra vender muito mais.",
+    image: "",
   },
   contentTwo: {
     title: "Pigz Gestão",
     description:
       "Acompanhe suas vendas em tempo real, no computador ou no celular, de onde estiver. Faça alterações de preços e disponibilidade de produtos rapidamente, como deve ser.",
+    image: "",
   },
 
   contentThree: {
     title: "Gestão de entregadores",
     description:
       "A ferramenta ideal pra quem tem entrega própria. Controle de entregas por motoboy, geração de relatórios por entregas e por taxas de entrega, individualmente.",
+    image: "",
   },
   contentFour: {
     title: "Pagamento on-line",
     description:
       "Segurança e agilidade para receber e entregar pedidos. Seus clientes pagam com Pix ou cartão de crédito pelo app, e o entregador nem precisa levar a maquininha de cartão.",
+    image: "",
   },
 };
